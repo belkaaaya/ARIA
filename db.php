5cfbd9179699d0384722da3b0bc73113c415a7a8
Belkacem Aya <aya.belkacem@univ-alger.dz>
2026-02-02 21:51:37 +0100

    Add files via upload

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "coffeeapp";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Erreur de connexion : " . $conn->connect_error);
+}
+?>
